9dd28707354449e3d11b08a1540da4d541bb93e4
arumdyaharum <arumdyaharum@gmail.com>
2022-03-03 13:40:22 +0700

    feat: slicing home page done

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,30 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import specific icons */
+import { 
+  faBars,
+  faMagnifyingGlass,
+  faArrowRight
+} from '@fortawesome/free-solid-svg-icons'
+import {  } from '@fortawesome/free-regular-svg-icons'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* add icons to the library */
+library.add(
+  faBars,
+  faMagnifyingGlass,
+  faArrowRight
+)
+
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
